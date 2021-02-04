faad1d2659bab0183f86dfc53bd7984105209d51
brian-trann <70680670+brian-trann@users.noreply.github.com>
2021-02-04 12:38:36 -0800

    add instance methods and modify templetes

--- models/customer.js
+++ models/customer.js
@@ -1,83 +1,88 @@
 /** Customer for Lunchly */
 
-const db = require("../db");
-const Reservation = require("./reservation");
+const db = require('../db');
+const Reservation = require('./reservation');
 
 /** Customer of the restaurant. */
 
 class Customer {
-  constructor({ id, firstName, lastName, phone, notes }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone;
-    this.notes = notes;
-  }
+	constructor({ id, firstName, lastName, phone, notes }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.notes = notes;
+	}
 
-  /** find all customers. */
+	/** find all customers. */
 
-  static async all() {
-    const results = await db.query(
-      `SELECT id, 
+	static async all() {
+		const results = await db.query(
+			`SELECT id, 
          first_name AS "firstName",  
          last_name AS "lastName", 
          phone, 
          notes
        FROM customers
        ORDER BY last_name, first_name`
-    );
-    return results.rows.map(c => new Customer(c));
-  }
+		);
+		return results.rows.map((c) => new Customer(c));
+	}
 
-  /** get a customer by ID. */
+	/** get a customer by ID. */
 
-  static async get(id) {
-    const results = await db.query(
-      `SELECT id, 
+	static async get(id) {
+		const results = await db.query(
+			`SELECT id, 
          first_name AS "firstName",  
          last_name AS "lastName", 
          phone, 
          notes 
         FROM customers WHERE id = $1`,
-      [id]
-    );
+			[ id ]
+		);
 
-    const customer = results.rows[0];
+		const customer = results.rows[0];
 
-    if (customer === undefined) {
-      const err = new Error(`No such customer: ${id}`);
-      err.status = 404;
-      throw err;
-    }
+		if (customer === undefined) {
+			const err = new Error(`No such customer: ${id}`);
+			err.status = 404;
+			throw err;
+		}
 
-    return new Customer(customer);
-  }
+		return new Customer(customer);
+	}
 
-  /** get all reservations for this customer. */
+	/** get all reservations for this customer. */
 
-  async getReservations() {
-    return await Reservation.getReservationsForCustomer(this.id);
-  }
+	async getReservations() {
+		return await Reservation.getReservationsForCustomer(this.id);
+	}
 
-  /** save this customer. */
+	/** save this customer. */
 
-  async save() {
-    if (this.id === undefined) {
-      const result = await db.query(
-        `INSERT INTO customers (first_name, last_name, phone, notes)
+	async save() {
+		if (this.id === undefined) {
+			const result = await db.query(
+				`INSERT INTO customers (first_name, last_name, phone, notes)
              VALUES ($1, $2, $3, $4)
              RETURNING id`,
-        [this.firstName, this.lastName, this.phone, this.notes]
-      );
-      this.id = result.rows[0].id;
-    } else {
-      await db.query(
-        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+				[ this.firstName, this.lastName, this.phone, this.notes ]
+			);
+			this.id = result.rows[0].id;
+		} else {
+			await db.query(
+				`UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
              WHERE id=$5`,
-        [this.firstName, this.lastName, this.phone, this.notes, this.id]
-      );
-    }
-  }
+				[ this.firstName, this.lastName, this.phone, this.notes, this.id ]
+			);
+		}
+	}
+
+	/** fullName() - returns a string of the first and last name */
+	fullName() {
+		return this.firstName + ' ' + this.lastName;
+	}
 }
 
 module.exports = Customer;
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Springboard Exercises
+## Section 35.3 Express Lunchly
+
+* Exercise working with Express.js, Node OOP pg models, and nunjucks 
